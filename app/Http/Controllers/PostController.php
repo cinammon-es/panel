4a6745248d32d440b881444246759192ec3c527a
lz20r <zhiyaonaiara@gmail.com>
2025-05-08 21:09:03 +0200

    Agregar validación y manejo de imágenes en la creación y actualización de publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,7 +32,22 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'picture' => 'nullable|image|max:2048',
+        ]);
+
+        $data = $request->only('title', 'content');
+        if ($request->hasFile('picture')) {
+            $file = $request->file('picture');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $path= $file->storeAs('uploads', $fileName, 'public');
+            $data['picture'] = '/storage/' . $path;
+        }
+
+        Post::create($data);
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 
     /**
@@ -54,16 +69,35 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+
+    /**
+     * solo quiero que se actualice el contenido y la imagen no que se cree uno nuevo
+     */
+    public function update(Request $request, Post $post) {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'picture' => 'nullable|image|max:2048',
+        ]);
+
+        $data = $request->only('title', 'content');
+        if ($request->hasFile('picture')) {
+            $file = $request->file('picture');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $path = $file->storeAs('uploads', $fileName, 'public');
+            $data['picture'] = '/storage/' . $path;
+        }
+        $post->update($data);
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
+    } 
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
     }
 }
