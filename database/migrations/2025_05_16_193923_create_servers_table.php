c06978c45671b9ea9fa163888c0fc3e38e18c28e
lz20r <zhiyaonaiara@gmail.com>
2025-05-17 00:11:14 +0200

    feat: Agregar columna 'egg_id' y clave foránea en la tabla 'servers'

--- database/migrations/2025_05_16_193923_create_servers_table.php
+++ database/migrations/2025_05_16_193923_create_servers_table.php
@@ -8,9 +8,10 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('servers', function (Blueprint $table) {
-            $table->id(); // PRIMARY KEY BIGINT UNSIGNED
+            $table->id(); // BIGINT UNSIGNED PRIMARY KEY
             $table->string('uuid', 36)->unique();
             $table->string('uuidShort', 8)->unique();
+
             $table->unsignedMediumInteger('node');
             $table->string('name');
             $table->boolean('active');
@@ -20,6 +21,8 @@ return new class extends Migration {
             $table->unsignedInteger('disk');
             $table->unsignedInteger('io');
             $table->unsignedInteger('cpu');
+            $table->unsignedBigInteger('egg_id')->nullable();
+            $table->foreign('egg_id')->references('id')->on('eggs')->onDelete('set null');
             $table->boolean('oom_disabled')->default(false);
             $table->string('ip');
             $table->unsignedInteger('port');
@@ -29,12 +32,17 @@ return new class extends Migration {
             $table->string('daemonSecret', 36)->unique();
             $table->string('username')->unique();
             $table->boolean('installed')->default(false);
+
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
+        Schema::table('servers', function (Blueprint $table) {
+            $table->dropForeign(['egg_id']);
+        });
+
         Schema::dropIfExists('servers');
     }
 };
