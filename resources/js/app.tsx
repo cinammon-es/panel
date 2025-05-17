406d25a3b90542128a2632555d3eedf4ec6e9e94
lz20r <zhiyaonaiara@gmail.com>
2025-05-17 03:41:27 +0200

    feat: Agregar importación de bootstrap-icons en app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,6 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
