ced433e36ceb853db0a4eb7a9380cf92a6562761
lz20r <zhiyaonaiara@gmail.com>
2025-05-24 01:57:41 +0200

    feat: Agregar ruta para la gestión de roles en el controlador de roles

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\NodeController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\ServerController;
 use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\RoleController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -38,6 +39,7 @@ Route::delete('/tickets/{ticket}/messages/{message}', [TicketController::class, 
 Route::resource('eggs', EggController::class);
 Route::resource('nodes', NodeController::class);
 Route::resource('servers', ServerController::class);
+Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
 
 
 require __DIR__.'/settings.php';
